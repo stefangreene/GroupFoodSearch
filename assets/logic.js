1500e5c9da6df61f6132503b80e3ab50d963560e
Cecilia Pasquale <cecilia.pasquale16@gmail.com>
2019-02-07 19:11:39 -0500

    begin fb

--- assets/logic.js
+++ assets/logic.js
@@ -212,5 +212,28 @@ function yummlyRecipe2(RecipeIdPar){
                          
                 });
     }
+//FIREBASE FUNCTION
+function contactScript(value) {
+    var a = {
 
- 
+        // Initialize Firebase
+        apiKey: "AIzaSyBN-RamSUys4RiUKy7k4taVK-uu8Db5cSk",
+        authDomain: "groupfoodsearch.firebaseapp.com",
+        databaseURL: "https://groupfoodsearch.firebaseio.com",
+        projectId: "groupfoodsearch",
+        storageBucket: "groupfoodsearch.appspot.com",
+        messagingSenderId: "636824267861"
+    };
+    firebase.initializeApp(config);
+    var b = firebase.database().ref("messages");
+    $("#newContact").submit(function (a) {
+    $(this), console.log("Submit to Firebase");
+        var c = $("#name").val(),
+            d = $("#email").val(),
+            f = { name: c, email: d };
+        return b.push(f).then(function (a) {
+            $(".sucess").css("display", "block"),
+                $(".sucess-none").css("display", "none")
+        }), !1
+    })
+}
